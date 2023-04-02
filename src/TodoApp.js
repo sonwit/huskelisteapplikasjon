a27a1f034c643e5660fa14f8d9f9d02d9e558646
Sondre Wittek <sondrewittek@gmail.com>
2023-04-02 20:34:43 +0200

    init with chatGPT generated code

---
+++ src/TodoApp.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (input.trim() === "") return;
+    setTodos([...todos, { text: input, completed: false }]);
+    setInput("");
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const toggleComplete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  const filterCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleComplete(index)}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={filterCompleted}>Filter Completed</button>
+    </div>
+  );
+};
+
+export default TodoApp;
