e55de8b7b272ca837a4f731f4e38efc820ca0887
Sondre Wittek <sondrewittek@gmail.com>
2023-04-02 22:34:26 +0200

    fix styling + accessability

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+  const [showCompleted, setShowCompleted] = useState(false);
+  const [statusMessage, setStatusMessage] = useState("");
 
   const addTodo = (e) => {
     e.preventDefault();
     if (input.trim() === "") return;
     setTodos([...todos, { text: input, completed: false }]);
+    setStatusMessage(`Oppgaven: ${input} ble lagt til`);
     setInput("");
   };
 
   const deleteTodo = (index) => {
+    setStatusMessage(
+      `Oppgaven: ${todos[index]?.text || "ukjent"} ble lagt slettet`
+    );
     setTodos(todos.filter((_, i) => i !== index));
   };
 
@@ -21,41 +27,77 @@ const TodoApp = () => {
     setTodos(updatedTodos);
   };
 
-  const filterCompleted = () => {
-    setTodos(todos.filter((todo) => !todo.completed));
-  };
+  const filterdTodos = useMemo(() => {
+    if (showCompleted) return todos;
+    return todos.filter((todo) => !todo.completed);
+  }, [todos, showCompleted]);
 
   return (
     <div>
-      <h1>Todo App</h1>
+      <h1>Huskeliste</h1>
+
+      <div role="status" aria-live="polite" className="vh">
+        {/* <!-- add content to hear it spoken --> */}
+        {statusMessage}
+      </div>
+
+      <ul>
+        {filterdTodos.map((todo, index) => {
+          const itemId = `todo-${index}`;
+          return (
+            <li key={index}>
+              <input
+                className="vh"
+                id={itemId}
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => toggleComplete(index)}
+              />
+              <label htmlFor={itemId} className="text">
+                <span class="tick"></span>
+                <span class="text">{todo.text}</span>
+              </label>
+              <button
+                onClick={() => deleteTodo(index)}
+                aria-label={`slett ${todo.text}`}
+              >
+                &times;
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      {filterdTodos.length === 0 && (
+        <div className="empty-state">
+          <p>
+            Enten har du allerede gjort alt du skal, eller så er det fortsatt
+            ting å legge til på listen din. Legg til din første oppgave &#x2193;
+          </p>
+        </div>
+      )}
+      <button
+        className="toggle-btn"
+        aria-pressed={showCompleted}
+        onClick={() => setShowCompleted(!showCompleted)}
+      >
+         <span class="tick"></span>
+        <span class="text"> Vis fullførte oppgaver</span>
+
+      </button>
       <form onSubmit={addTodo}>
+        <label className="vh" htmlFor="addTodoName">
+          Legg til ny oppgave
+        </label>
         <input
+          id="addTodoName"
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          placeholder="Legg til ny oppgave"
         />
-        <button type="submit">Add</button>
+
+        <button type="submit">Legg til</button>
       </form>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => toggleComplete(index)}
-            />
-            <span
-              style={{
-                textDecoration: todo.completed ? "line-through" : "none",
-              }}
-            >
-              {todo.text}
-            </span>
-            <button onClick={() => deleteTodo(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={filterCompleted}>Filter Completed</button>
     </div>
   );
 };
